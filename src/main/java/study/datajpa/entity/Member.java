34f73d01dd7b706abe0dfd953f9ea33c2e520376
kangjjs <emdtod12@naver.com>
2023-01-18 23:04:27 +0900

    Second Commit

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,34 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"})
+@NamedQuery(
+        name="Member.findByUsername",
+        query="select m from Member m where m.username = :username")
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name ="member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
